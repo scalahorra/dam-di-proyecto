78d5b843c837d64e528be9399703a53a4a1f5cb3
sergi <sergi@DESKTOP-Q89AULR.home>
2021-05-23 02:18:03 +0200

    conseguir los datos de los campos del loggin

--- src/main/java/gui/CreateUser.java
+++ src/main/java/gui/CreateUser.java
@@ -1,27 +1,47 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+
+import constructores.ClienteBuilder;
+import dao.ClienteDao;
+import excepciones.ExcepcionDNI;
+import excepciones.ExcepcionDuplicidad;
+import excepciones.ExcepcionEmail;
+import excepciones.ExcepcionNIE;
+import modelo.Cliente;
+import modelo.Cliente.TipoDocumento;
+
 import javax.swing.JScrollBar;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JRadioButton;
 
 public class CreateUser {
-
+	
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField txtNombre;
+	private JTextField txtTelefono;
+	private JTextField txtFax;
+	private JTextField txtEmail;
+	private JTextField txtPassword;
+	private JTextField txtDireccion;
+	private JTextField txtDocumento;
+	private ButtonGroup grupoRBotones = new ButtonGroup();
+	private JTextField txtCiudad;
+	private JTextField txtCodigoCliente;
 
 	/**
 	 * Launch the application.
@@ -65,20 +85,20 @@ public class CreateUser {
 		lblNombreCliente.setBounds(10, 72, 90, 14);
 		frame.getContentPane().add(lblNombreCliente);
 		
-		textField = new JTextField();
-		textField.setBounds(110, 69, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtNombre = new JTextField();
+		txtNombre.setBounds(110, 69, 86, 20);
+		frame.getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
 		
 		// Telefono
 		JLabel lblTelefono = new JLabel("Tel\u00E9fono");
 		lblTelefono.setBounds(10, 125, 60, 14);
 		frame.getContentPane().add(lblTelefono);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(110, 122, 86, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		txtTelefono = new JTextField();
+		txtTelefono.setBounds(110, 122, 86, 20);
+		frame.getContentPane().add(txtTelefono);
+		txtTelefono.setColumns(10);
 		
 		
 		// Fax
@@ -86,73 +106,115 @@ public class CreateUser {
 		lblFax.setBounds(10, 178, 46, 14);
 		frame.getContentPane().add(lblFax);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(110, 175, 86, 20);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		txtFax = new JTextField();
+		txtFax.setBounds(110, 175, 86, 20);
+		frame.getContentPane().add(txtFax);
+		txtFax.setColumns(10);
 		
 		// Email
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setBounds(10, 231, 46, 14);
 		frame.getContentPane().add(lblEmail);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(110, 228, 86, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		txtEmail = new JTextField();
+		txtEmail.setBounds(110, 228, 86, 20);
+		frame.getContentPane().add(txtEmail);
+		txtEmail.setColumns(10);
 		
 		// Password
 		JLabel lblPassword = new JLabel("Contrase\u00F1a");
 		lblPassword.setBounds(253, 72, 80, 14);
 		frame.getContentPane().add(lblPassword);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(360, 69, 86, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		txtPassword = new JTextField();
+		txtPassword.setBounds(360, 69, 86, 20);
+		frame.getContentPane().add(txtPassword);
+		txtPassword.setColumns(10);
 		
 		// Direccion
 		JLabel lblDireccion = new JLabel("Direcci\u00F3n");
 		lblDireccion.setBounds(253, 125, 60, 14);
 		frame.getContentPane().add(lblDireccion);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(360, 122, 86, 20);
-		frame.getContentPane().add(textField_5);
-		textField_5.setColumns(10);
+		txtDireccion = new JTextField();
+		txtDireccion.setBounds(360, 122, 86, 20);
+		frame.getContentPane().add(txtDireccion);
+		txtDireccion.setColumns(10);
 		
 		// Ciudad
 		JLabel lblCiudad = new JLabel("Ciudad");
 		lblCiudad.setBounds(253, 178, 46, 14);
 		frame.getContentPane().add(lblCiudad);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Madrid", "Barcelona", "Valencia", "Sevilla", "Zaragoza", "M\u00E1laga", "Murcia", "Palma", "Las Palmas", "Bilbao"}));
-		comboBox.setBounds(360, 174, 86, 22);
-		frame.getContentPane().add(comboBox);
-		
 		// Tipo documento
 		JLabel lblTipoDocumento = new JLabel("Tipo documento");
 		lblTipoDocumento.setBounds(300, 231, 95, 14);
 		frame.getContentPane().add(lblTipoDocumento);
 		
-		JCheckBox chckbxDNI = new JCheckBox("DNI");
-		chckbxDNI.setBounds(263, 252, 50, 23);
-		frame.getContentPane().add(chckbxDNI);
+		// RBoton DNI
+		JRadioButton rdbtnDNI = new JRadioButton("DNI");
+		rdbtnDNI.setBounds(258, 252, 109, 23);
+		grupoRBotones.add(rdbtnDNI);
+		frame.getContentPane().add(rdbtnDNI);
 		
-		JCheckBox chckbxNIE = new JCheckBox("NIE");
-		chckbxNIE.setBounds(381, 252, 97, 23);
-		frame.getContentPane().add(chckbxNIE);
+		// RBoton NIE
+		JRadioButton rdbtnNIE = new JRadioButton("NIE");
+		rdbtnNIE.setBounds(369, 252, 109, 23);
+		grupoRBotones.add(rdbtnNIE);
+		frame.getContentPane().add(rdbtnNIE);
+		
+		txtDocumento = new JTextField();
+		txtDocumento.setBounds(298, 282, 86, 20);
+		frame.getContentPane().add(txtDocumento);
+		txtDocumento.setColumns(10);
+		
+		txtCiudad = new JTextField();
+		txtCiudad.setBounds(360, 175, 86, 20);
+		frame.getContentPane().add(txtCiudad);
+		txtCiudad.setColumns(10);
+		
+		JLabel lblCodigoCliente = new JLabel("C\u00F3digo cliente");
+		lblCodigoCliente.setBounds(10, 285, 90, 14);
+		frame.getContentPane().add(lblCodigoCliente);
+		
+		txtCodigoCliente = new JTextField();
+		txtCodigoCliente.setBounds(110, 282, 86, 20);
+		frame.getContentPane().add(txtCodigoCliente);
+		txtCodigoCliente.setColumns(10);
+		
+		
+		
+		
 		
 		JButton btnCrear = new JButton("CREAR");
-		btnCrear.setBounds(107, 329, 89, 23);
+		btnCrear.setBounds(107, 390, 89, 23);
 		frame.getContentPane().add(btnCrear);
+		btnCrear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					
+					Cliente c1 = ClienteBuilder.buildCliente(Integer.parseInt(txtCodigoCliente.getText()), txtNombre.getText(),
+							txtTelefono.getText(), txtFax.getText(), txtDireccion.getText(), txtCiudad.getText(),
+							txtEmail.getText(), txtPassword.getText(), null, txtDocumento.getText());
+					
+					clienteDao.guardar(c1);
+					
+					for(Cliente c : clientes) {
+						System.out.println(c.toString());
+					}
+					
+				} catch (NumberFormatException | ExcepcionDuplicidad | ExcepcionEmail | ExcepcionDNI
+						| ExcepcionNIE e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 		
 		JButton btnBorrar = new JButton("BORRAR");
-		btnBorrar.setBounds(253, 329, 89, 23);
-		frame.getContentPane().add(btnBorrar);
-		
-		
-		
+		btnBorrar.setBounds(244, 390, 89, 23);
+		frame.getContentPane().add(btnBorrar);				
 	}
+	
+	List<Cliente> clientes = new ArrayList<Cliente>();
+	ClienteDao clienteDao = new ClienteDao(clientes);
 }
