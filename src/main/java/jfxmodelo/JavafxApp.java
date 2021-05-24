a0256f9a36aef030aab4bbe2faeeb4c43d141551
sergi <sergi@DESKTOP-Q89AULR.home>
2021-05-25 00:28:19 +0200

    jaspersoft

---
+++ src/main/java/jfxmodelo/JavafxApp.java
@@ -0,0 +1,18 @@
+package jfxmodelo;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JavafxApp {
+
+	
+	public static void main(String[] args) {
+		JavafxMain.main(args);
+	}
+	
+}
